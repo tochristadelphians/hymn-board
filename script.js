d7710a20968a1105d7a0af5938a366399bc2a257
tochristadelphians <tochristadelphians@gmail.com>
2026-03-13 18:28:49 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const sheetURL = "https://docs.google.com/spreadsheets/d/1MmmFbv14UMZZ-4OsKwYRXvwOlTKeUy8q4x-DRAflQtg/gviz/tq?tqx=out:json&gid=0";
-
-fetch(sheetURL)
-.then(function(res){
-return res.text();
-})
-.then(function(text){
-console.log(text);
-});
