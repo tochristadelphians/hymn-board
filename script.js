b3d3319722b9f05b0be00d64121824960ccb9322
tochristadelphians <tochristadelphians@gmail.com>
2026-03-13 18:27:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,9 @@
 const sheetURL = "https://docs.google.com/spreadsheets/d/1MmmFbv14UMZZ-4OsKwYRXvwOlTKeUy8q4x-DRAflQtg/gviz/tq?tqx=out:json&gid=0";
 
-function updateBoard() {
-
-fetch(sheetURL + "&t=" + Date.now())
-.then(function(response){
-return response.text();
+fetch(sheetURL)
+.then(function(res){
+return res.text();
 })
 .then(function(text){
-
-var json = JSON.parse(text.substring(47, text.length-2));
-var rows = json.table.rows;
-
-function get(r,c){
-if(!rows[r] || !rows[r].c[c]) return "";
-return rows[r].c[c].f || rows[r].c[c].v || "";
-}
-
-document.getElementById("label1").innerText = get(1,0);
-document.getElementById("h1").innerText = get(1,1);
-
-document.getElementById("label2").innerText = get(2,0);
-document.getElementById("psalm").innerText = get(2,1);
-
-document.getElementById("label3").innerText = get(3,0);
-document.getElementById("reading").innerText = get(3,1);
-
-document.getElementById("label4").innerText = get(4,0);
-document.getElementById("h2").innerText = get(4,1);
-
-document.getElementById("label5").innerText = get(5,0);
-document.getElementById("h3").innerText = get(5,1);
-
-document.getElementById("label6").innerText = get(6,0);
-document.getElementById("h4").innerText = get(6,1);
-
-document.getElementById("verse").innerText = get(7,1);
-document.getElementById("reference").innerText = get(8,1);
-
-document.body.style.backgroundImage =
-"linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.4)), url(" + get(9,1) + ")";
-
-})
-.catch(function(error){
-console.log(error);
+console.log(text);
 });
-
-}
-
-updateBoard();
-setInterval(updateBoard,30000);
